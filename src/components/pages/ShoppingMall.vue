24396919731bed10e91ce148f159ab8bf8f58f4f
MrLi <xiansheng0709@gmail.com>
2018-12-07 22:38:16 +0800

    update

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <van-row class="test-row">
-      <van-col span="4">span:4</van-col>
-      <van-col span="16">span:16</van-col>
-      <van-col span="4">span:4</van-col>
-    </van-row>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">icon</van-col>
+        <van-col span="16">search input</van-col>
+        <van-col span="5">button</van-col>
+      </van-row>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      msg: "Shopping Mall"
-    };
-  }
-};
+  export default {
+    data() {
+      return {
+        msg: "Shopping Mall"
+      };
+    }
+  };
 </script>
 
 <style scoped lang="less">
-  .test-row{
-    text-align: center;
+  .search-bar{
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
   }
 </style>
