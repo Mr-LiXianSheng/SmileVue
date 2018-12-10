4a6251d17290b7d83ec6e8e4aea5a29f4b348048
MrLi <xiansheng0709@gmail.com>
2018-12-10 22:41:20 +0800

    update

--- service/index.js
+++ service/index.js
@@ -5,6 +5,14 @@ app.use(async (ctx) => {
   ctx.body = '<h1>hello Koa2</h1>'
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('[Server] starting at port 3000')
 });
+
+//引入connect
+const {connect} = require('./database/init.js')
+
+//立即执行函数
+;(async () => {
+  await connect()
+})();
